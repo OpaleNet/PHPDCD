54c5d68842d9b82687ae7b400db733f4179328ce
yann <yann@opale.net>
2015-09-28 12:38:51 +0200

    Fixe missing line returns

--- engine.php
+++ engine.php
@@ -7,14 +7,14 @@ use Opale\PHPDCD\Formatter;
 use \Opale\PHPDCD\Writers\DefaultWriter;
 use \Opale\PHPDCD\Writers\ErrorWriter;
 
-set_error_handler('error_handler');
+set_error_handler(
+    function ($errorNumber, $errorMessage, $errorFile, $errorLine) {
+        throw new ErrorException($errorMessage, $errorNumber, $errorNumber, $errorFile, $errorLine);
+    }
+);
 date_default_timezone_set('UTC');
 ini_set('memory_limit', -1);
 
-function error_handler($errno, $errstr, $errfile, $errline)
-{
-    throw new ErrorException($errstr, $errno, $errno, $errfile, $errline);
-}
 
 $config = json_decode(file_get_contents('/config.json'), true);
 $engine = new Engine($config, new Formatter(), new DefaultWriter(), new ErrorWriter());
